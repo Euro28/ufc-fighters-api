3dbb60b7b0afc6cff39a9ec84d9dbc8550fc8ac8
Euro28 <eurol197628@gmail.com>
2021-08-25 08:43:58 +0100

    page content

--- pages/documentation.js
+++ pages/documentation.js
@@ -1,7 +1,21 @@
 export default function Documentation() {
   return (
     <div>
-      <body>This is the documentation page</body>
+      <body>
+        This is the documentation page
+        <h2>Getting Started</h2>
+        <p>
+          First make an API call this is done by querying the endpoint and
+          passing in the name of the fighter like so, the exact format can be
+          done using the search function.
+        </p>
+      </body>
+
+      <h2>Fields</h2>
+      <p>
+        The data can be queried on a individual fighter basis, or an event
+        basis, or even a per country basis
+      </p>
     </div>
   );
 }

69df4803390528bf8853d3d9f01ae7f341a64688
Euro28 <eurol197628@gmail.com>
2021-08-26 19:44:29 +0100

    testing

--- db/models/index.js
+++ db/models/index.js
@@ -13,6 +13,7 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
+  dialectModule: require("pg"),
   dialectOptions: {
     ssl: {
       require: true,

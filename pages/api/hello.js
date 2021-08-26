1586fda93ad7bbe8c25c445ea516987878145310
Euro28 <eurol197628@gmail.com>
2021-08-26 18:05:12 +0100

    mma api for name

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,32 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import db from "../../db/models/index.js";
 import Sequelize from "sequelize";
+import mma from '../../../mma-api/lib/mma/index.js'
 
-const { Op } = Sequelize;
-db.sequelize.sync();
-const Fight = db.fights;
+//const { Op } = Sequelize;
+//db.sequelize.sync();
+//const Fight = db.fights;
 
-const test = async () => {
-  const cannonier = await Fight.findAll({
-    where: {
-      [Op.or]: [
-        {
-          red_fighter: {
-            [Op.iLike]: "%jared cannonier",
-          },
-        },
-        {
-          blue_fighter: {
-            [Op.iLike]: "%jared cannonier",
-          },
-        },
-      ],
-    },
-  });
-  console.log(JSON.stringify(cannonier, null, 2));
-};
+//const test = async () => {
+//const cannonier = await Fight.findAll({
+//where: {
+//[Op.or]: [
+//{
+//red_fighter: {
+//[Op.iLike]: "%jared cannonier",
+//},
+//},
+//{
+//blue_fighter: {
+//[Op.iLike]: "%jared cannonier",
+//},
+//},
+//],
+//},
+//});
+//console.log(JSON.stringify(cannonier, null, 2));
+//};
 
-test();
+//test();
+//
+//
+mma.fighter("Jon Jones", (data) => {
+  console.log(data);
+})
 
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });

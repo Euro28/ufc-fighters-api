256196ba21c661bf8793bfcbb829e1581ce30487
Euro28 <eurol197628@gmail.com>
2021-08-26 15:44:24 +0100

    database query fights by fighter

---
+++ db/models/index.js
@@ -0,0 +1,34 @@
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import fightModel from "./fight.js";
+
+/*hacky stuff to get dotenv to work*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  define: {
+    timestamps: false,
+  },
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.fights = fightModel(sequelize, Sequelize);
+
+export default db;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from "../../db/models/index.js";
+import Sequelize from "sequelize";
+
+const { Op } = Sequelize;
+db.sequelize.sync();
+const Fight = db.fights;
+
+const test = async () => {
+  const cannonier = await Fight.findAll({
+    where: {
+      [Op.or]: [
+        {
+          red_fighter: {
+            [Op.iLike]: "%jared cannonier",
+          },
+        },
+        {
+          blue_fighter: {
+            [Op.iLike]: "%jared cannonier",
+          },
+        },
+      ],
+    },
+  });
+  console.log(JSON.stringify(cannonier, null, 2));
+};
+
+test();
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }

bc07b627afb268b32ec428ef0d7955fb8c72b699
Euro28 <eurol197628@gmail.com>
2021-08-27 10:51:07 +0100

    prisma

--- pages/api/fighters/[fighter].js
+++ pages/api/fighters/[fighter].js
@@ -1,31 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-//import db from "../../../db/models/index.js";
-//import Sequelize from "sequelize";
+import prisma from "../_base.js"
+import Cors from "cors"
+
 
 export default async function handler(req, res) {
   let { fighter } = req.query;
-  //const { Op } = Sequelize;
-  //const Fight = db.fights;
-
-  //db.sequelize.sync();
-
-  //const fights = await Fight.findAll({
-  //where: {
-  //[Op.or]: [
-  //{
-  //red_fighter: {
-  //[Op.iLike]: `%${fighter}`,
-  //},
-  //},
-  //{
-  //blue_fighter: {
-  //[Op.iLike]: `%${fighter}`,
-  //},
-  //},
-  //],
-  //},
-  //});
-
-  //res.status(200).json(fights);
-  res.status(200).json({ nameGiven: fighter });
+  const fights = await prisma.fights.findMany();
+  res.status(200).json(fights);
 }

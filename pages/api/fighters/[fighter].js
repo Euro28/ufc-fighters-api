1d7557a4aa76bcc054089c0b2642aea3a2fe4e8c
Euro28 <eurol197628@gmail.com>
2021-08-27 11:26:17 +0100

    prisma api fight query

--- pages/api/fighters/[fighter].js
+++ pages/api/fighters/[fighter].js
@@ -1,8 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import prisma from "../_base.js"
+import prisma from "../_base.js";
 
 export default async function handler(req, res) {
   let { fighter } = req.query;
-  const fights = await prisma.fights.findMany();
+  const fights = await prisma.fights.findMany({
+    where: {
+      OR: [
+        {
+          red_fighter : {
+            equals: fighter,
+            mode: "insensitive",
+          },
+        },
+        {
+          blue_fighter: {
+            equals: fighter,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   res.status(200).json(fights);
 }

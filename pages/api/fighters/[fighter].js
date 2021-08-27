06a72bfe05d0311c5bc150589457740650b16ad1
Euro28 <eurol197628@gmail.com>
2021-08-27 09:38:43 +0100

    no api

--- pages/api/fighters/[fighter].js
+++ pages/api/fighters/[fighter].js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import db from "../../../db/models/index.js";
 import Sequelize from "sequelize";
-import mma from "../../../mma-api/lib/mma/index.js";
 
 export default async function handler(req, res) {
   let { fighter } = req.query;
@@ -27,8 +26,5 @@ export default async function handler(req, res) {
     },
   });
 
-  mma.fighter(fighter, (data) => {
-    data.fights = fights;
-    res.status(200).json(data);
-  });
+  res.status(200).json(fights);
 }

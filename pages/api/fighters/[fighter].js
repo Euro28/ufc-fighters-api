aff9777557280de7d466bcb146c543e02e759da0
Euro28 <eurol197628@gmail.com>
2021-08-27 09:45:25 +0100

    no database

--- pages/api/fighters/[fighter].js
+++ pages/api/fighters/[fighter].js
@@ -1,30 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import db from "../../../db/models/index.js";
-import Sequelize from "sequelize";
+//import db from "../../../db/models/index.js";
+//import Sequelize from "sequelize";
 
 export default async function handler(req, res) {
-  let { fighter } = req.query;
-  const { Op } = Sequelize;
-  const Fight = db.fights;
+  //let { fighter } = req.query;
+  //const { Op } = Sequelize;
+  //const Fight = db.fights;
 
-  db.sequelize.sync();
+  //db.sequelize.sync();
 
-  const fights = await Fight.findAll({
-    where: {
-      [Op.or]: [
-        {
-          red_fighter: {
-            [Op.iLike]: `%${fighter}`,
-          },
-        },
-        {
-          blue_fighter: {
-            [Op.iLike]: `%${fighter}`,
-          },
-        },
-      ],
-    },
-  });
+  //const fights = await Fight.findAll({
+  //where: {
+  //[Op.or]: [
+  //{
+  //red_fighter: {
+  //[Op.iLike]: `%${fighter}`,
+  //},
+  //},
+  //{
+  //blue_fighter: {
+  //[Op.iLike]: `%${fighter}`,
+  //},
+  //},
+  //],
+  //},
+  //});
 
-  res.status(200).json(fights);
+  //res.status(200).json(fights);
+  res.status(200).json({ testData: "testing new" });
 }

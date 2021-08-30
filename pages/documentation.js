2d4a65902c94051f5e3088ddfc6ee8b05180400b
Euro28 <eurol197628@gmail.com>
2021-08-30 15:57:09 +0100

    documentation

--- pages/documentation.js
+++ pages/documentation.js
@@ -1,21 +1,101 @@
+import { Box, Heading, Container, Center } from "@chakra-ui/react";
+import initial from "../public/initialFighter.json";
 export default function Documentation() {
   return (
-    <div>
+    <>
       <body>
-        This is the documentation page
-        <h2>Getting Started</h2>
-        <p>
-          First make an API call this is done by querying the endpoint and
-          passing in the name of the fighter like so, the exact format can be
-          done using the search function.
-        </p>
+        <Box height="1000px" backgroundImage="/background.jpg">
+          <Container
+            backgroundColor="#F0EEE1"
+            maxWidth="66.666%"
+            fontFamily="Tahoma, serif"
+            color="#333"
+          >
+            <Heading as="h3" size="md" fontFamily="Tahoma" marginBottom="10px">
+              Fighters
+            </Heading>
+            <p>A fighter that has at some point in time fought in the UFC.</p>
+            <b>Example request</b>
+            <pre
+              style={{
+                backgroundColor: "white",
+                marginTop: "10px",
+                marginBottom: "10px",
+              }}
+            >
+              https://ufc-fighters-api.vercel.app/api/fighters/Jon-jones
+            </pre>
+            <b>Example Response</b>
+            <pre style={{ marginTop: "10px", marginBottom: "10px" }}>
+              {JSON.stringify(initial, null, 8)}
+            </pre>
+            <b>Attributes</b>
+            <ul style={{ marginLeft: "44px", marginTop: "10px" }}>
+              <li>
+                <code>name </code>
+                <em>string </em>
+                -- The name of the fighter.
+              </li>
+              <li>
+                <code>nickname </code>
+                <em>string </em>
+                -- The nickname of the fighter.
+              </li>
+              <li>
+                <code>association </code>
+                <em>string </em>
+                -- The gym which the fighter trains at
+              </li>
+              <li>
+                <code>age </code>
+                <em>integer </em>
+                -- The age of the fighter
+              </li>
+              <li>
+                <code>birthday </code>
+                <em>date </em>
+                -- The birthday of the fighter
+              </li>
+              <li>
+                <code>nationality </code>
+                <em>string </em>
+                -- The nationality of the fighter
+              </li>
+              <li>
+                <code>height </code>
+                <em>string </em>
+                -- The height in feet and inches of the fighter
+              </li>
+              <li>
+                <code>weight </code>
+                <em>string </em>
+                -- The weight in pounds of the fighter
+              </li>
+              <li>
+                <code>weight_class </code>
+                <em>string </em>
+                -- The weight class in the UFC in which the fighter competes
+              </li>
+              <li>
+                <code>wins </code>
+                <em>object </em>
+                -- An object breaking down the different ways the fighter has
+                won.
+              </li>
+              <li>
+                <code>losses </code>
+                <em>object </em>
+                -- An object detailing the all the losses the fighter has.
+              </li>
+              <li>
+                <code>fights </code>
+                <em>array </em>
+                -- An array which contains every fight the fighter has had.
+              </li>
+            </ul>
+          </Container>
+        </Box>
       </body>
-
-      <h2>Fields</h2>
-      <p>
-        The data can be queried on a individual fighter basis, or an event
-        basis, or even a per country basis
-      </p>
-    </div>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import RequestMaker from "../components/requestMaker";
 export default function Home() {
   return (
     <>
-      <body style={{ background: "#222" }}>
+      <body >
         <Box height="1000px" backgroundImage="/background.jpg">
           <Container
             backgroundColor="#F0EEE1"
@@ -12,25 +12,6 @@ export default function Home() {
             fontFamily="Tahoma, serif"
             color="#333"
           >
-            <Heading as="h3" size="lg" fontFamily="Tahoma" marginBottom="10px">
-              How do I use this?
-            </Heading>
-            <p >
-              All the fighter information can be accessed throgh the REST API,
-              take a look at the documentation if you need any help. You can try
-              querying the API with the search bar below, simply input a name
-              and the api will try to match the most popular fighter, for
-              example if you input khabib althrough there are multiple in the
-              UFC only Khabib Nurmagamedov will be chosen. For the sake of
-              brevity only the most recent fight is shown in the below helper
-              tool. While the API response will contain all fights from mid
-              2010s
-            </p>
-            <RequestMaker />
-            <Heading as="h3" size="md" fontFamily="Tahoma">
-              CopyRight
-            </Heading>
-            <p>The main fight dataset is from kaggle listed here</p>
             <Heading as="h3" size="md" fontFamily="Tahoma">
               My Motivation for making this?
             </Heading>
@@ -38,10 +19,43 @@ export default function Home() {
               Personally as a huge fan of MMA and sports betting as well, I have
               gut feelings about certain fighters, and questions like the win
               percentage of fighters as they age, relative to the age of their
-              opponents and whether a wrestling or striking style ages better,
-              the database currently only contains fight from around early to
-              mid 2010s onwards, possible future work more organisations could
-              be added or even boxers could also be added.{" "}
+              opponents and whether a wrestling or striking style ages better
+              are hard to answer so hopefully this API could give some hints
+              towards the answers. the database currently only contains fight
+              from around early to mid 2010s onwards, possible future work could
+              include more organisations be added or even boxers could also be
+              added.{" "}
+            </p>
+            <Heading as="h3" size="md" fontFamily="Tahoma" marginBottom="10px">
+              How do I use this?
+            </Heading>
+            <p>
+              All the fighter information can be accessed throgh the REST API,
+              take a look at the documentation if you need any help. You can try
+              querying the API with the search bar below, simply input a name
+              and the api will try to match the most popular fighter, for
+              example if you input khabib althrough there are multiple in the
+              UFC only Khabib Nurmagamedov will be chosen. For the sake of
+              brevity only the most recent fight is shown in the below helper
+              tool. While the API response will contain all fights the fighter
+              has had in the UFC.
+            </p>
+            <RequestMaker />
+            <Heading as="h3" size="md" fontFamily="Tahoma">
+              CopyRight
+            </Heading>
+            <p>
+              The main fight dataset is from kaggle listed{" "}
+              <a
+                href="https://www.kaggle.com/mdabbert/ultimate-ufc-dataset"
+                style={{ color: "blue" }}
+              >
+                here
+              </a>
+            </p>
+            <p>The project is also open source and carries an MIT license.</p>
+            <p>
+              Miscellaneous fighter information is web scraped from Sherdog.
             </p>
           </Container>
         </Box>
